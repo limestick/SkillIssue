989545baa8deec494026656d5df5ec0bba9ee067
limestick <notehaze@proton.me>
2024-09-20 21:47:33 +0200

    Implementing EntityDamageByEntityEvent #1

--- src/org/limestick/skillissue/listener/EntityDamageListener.java
+++ src/org/limestick/skillissue/listener/EntityDamageListener.java
@@ -1,20 +1,13 @@
 package org.limestick.skillissue.listener;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.limestick.skillissue.Main;
 
 public class EntityDamageListener implements Listener {
-	
-	public static Set<DamageCause> causes = new HashSet<DamageCause>(Arrays.asList(
-		     new DamageCause[] { DamageCause.ENTITY_ATTACK, DamageCause.PROJECTILE, DamageCause.ENTITY_EXPLOSION } 
-	));
 	
 	Main plugin;
 	
@@ -23,14 +16,18 @@ public class EntityDamageListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onEntityDamage(EntityDamageEvent e) {
-		if(!causes.contains(e.getCause())) return;
+	public void onEntityDamage(EntityDamageEvent event) {
+		if(!(event instanceof EntityDamageByEntityEvent)) return;
 		
-		int damage = (int) Math.floor(e.getDamage() * plugin.diff.damageMultiplier);
+		EntityDamageByEntityEvent e = (EntityDamageByEntityEvent) event;
 		
-		if (damage < 1 && e.getDamage() >= 1) { damage = 1; }
+		if(!(e.getEntity() instanceof Player) || e.getDamager() instanceof Player) return;
 		
-		e.setDamage(damage);
+		int d = (int) Math.floor(e.getDamage() * plugin.diff.damageMultiplier);
+		
+		if (d < 1 && e.getDamage() >= 1) d = 1;
+		
+		e.setDamage(d);
 	}
 	
 }
